c6972aece28a40fe43e2a57bc34d6abce8daef6b
Vinícius <nivi.tempest@gmail.com>
2021-02-27 00:46:33 -0300

    sticky header

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
+/* header-menu */
 function controlMenu(param) {
     let sideMenu = document.querySelector('.side-menu')
-    
+
     param == 'open'
         ? sideMenu.style.right = "0px"
         : sideMenu.style.right = "-300px"
 }
+
+/* fixed header */
+function fixedHeader() {
+    let header = document.querySelector('header')
+
+    if (window.scrollY > 900) {
+        header.classList.add('fixed')
+    }else{
+        header.classList.remove('fixed')
+    }
+}
+
+window.addEventListener('scroll', fixedHeader)
