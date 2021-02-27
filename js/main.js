b89eaf0d8c205e6e6c35f9d51da24809e0789494
Vinícius <nivi.tempest@gmail.com>
2021-02-27 01:40:48 -0300

    fix side-menu

--- js/main.js
+++ js/main.js
@@ -2,20 +2,31 @@
 function controlMenu(param) {
     let sideMenu = document.querySelector('.side-menu')
 
-    param == 'open'
-        ? sideMenu.style.right = "0px"
-        : sideMenu.style.right = "-300px"
+    if (param == 'open') {
+        sideMenu.style.right = "0px"
+    } else if (param == 'close') {
+        sideMenu.style.right = "-300px"
+    }
 }
+
+function resizeMenuHeader() {
+    let sideMenu = document.querySelector('.side-menu')
+    let bodyHeight = document.body.clientHeight
+
+    sideMenu.style.height = `${bodyHeight}px`
+}
+
+
+window.addEventListener("resize", resizeMenuHeader)
+window.addEventListener("load", resizeMenuHeader)
 
 /* fixed header */
 function fixedHeader() {
     let header = document.querySelector('header')
 
-    if (window.scrollY > 900) {
-        header.classList.add('fixed')
-    }else{
-        header.classList.remove('fixed')
-    }
+    window.scrollY > 900
+        ? header.classList.add('fixed')
+        : header.classList.remove('fixed')
 }
 
 window.addEventListener('scroll', fixedHeader)
